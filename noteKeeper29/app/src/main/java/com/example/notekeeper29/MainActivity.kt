bd8dd627664b957b12233723ece605e59d1ce52c
MihaiGaidau <gaidaumihai@gmail.com>
2020-03-15 23:02:23 +0200

    1.5.11 Demo: Retriving Intent Info an...

--- noteKeeper29/app/src/main/java/com/example/notekeeper29/MainActivity.kt
+++ noteKeeper29/app/src/main/java/com/example/notekeeper29/MainActivity.kt
@@ -7,6 +7,8 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
 import com.example.notekeeper29.model.CourseInfo
+import com.example.notekeeper29.model.EXTRA_NOTE_POSITION
+import com.example.notekeeper29.model.POSITION_NOT_SET
 
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
@@ -15,6 +17,7 @@ import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var notePosition = POSITION_NOT_SET
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +28,20 @@ class MainActivity : AppCompatActivity() {
         val adapterCourses = ArrayAdapter<CourseInfo>(this,
             android.R.layout.simple_spinner_item, DataManager.courses.values.toList())
         spinnerCourses.adapter = adapterCourses
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+        if (notePosition != POSITION_NOT_SET)
+            displayNote()
+
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes.get(notePosition)
+        textNoteTitle.setText(note.title)
+        textNoteText.setText(note.text)
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses.setSelection(coursePosition)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
